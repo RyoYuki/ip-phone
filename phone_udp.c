f0310907311cd7130a684a72ac22052bb03b4bf7
Takuma YOSHITANI <takuma.7@gmail.com>
2014-07-10 15:59:59 +0900

    add video stream function(still in progress)

--- phone_udp.c
+++ phone_udp.c
@@ -14,20 +14,27 @@
 
 #include <sys/ioctl.h>
 #include <sys/mount.h>
-/* #include <linux/fs.h> */
+#include <linux/fs.h>
 
-/* #include "fft.c" */
+#include <cv.h>
+#include <highgui.h>
+#include <stdint.h>
+
+#include "fft.c"
 
 #define PORT (50000)
 #define UDP_PORT (50001)
+#define VIDEO_PORT (50002)
 
 #define BUF_SIZE (1024)
 #define LPF_THRESHOLD (900)
 
 #define NOISE_THRESHOLD (10)
 
+#define CAP_SKIP_NUM (10)
+
 int main(int argc, char** argv){
-    int listen_fd, recv_fd=0, send_fd, audio_fd, audio_socket_fd, max_fd;
+    int listen_fd, recv_fd, send_fd, audio_fd, audio_socket_fd, video_socket_fd, max_fd;
     int isCalling = 0;
     int isMyCalling = 0;
     fd_set fds, readfds;
@@ -39,11 +46,13 @@ int main(int argc, char** argv){
     listen_fd = socket(PF_INET, SOCK_STREAM, 0);
 
     audio_socket_fd = socket(PF_INET, SOCK_DGRAM, 0);
+    vidoe_socket_fd = socket(PF_INET, SOCK_DGRAM, 0);
 
     socklen_t len = sizeof(struct sockaddr_in);
     socklen_t len_udp;
+    socklen_t len_v_udp;
 
-    struct sockaddr_in my_addr, other_recv_addr, other_send_addr, audio_addr;
+    struct sockaddr_in my_addr, other_recv_addr, other_send_addr, audio_addr, video_addr;
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(PORT);
     my_addr.sin_addr.s_addr = INADDR_ANY;
@@ -62,14 +71,35 @@ int main(int argc, char** argv){
     audio_addr.sin_family = AF_INET;
     audio_addr.sin_port = htons(UDP_PORT);
 
+    video_addr.sin_family = AF_INET;
+    video_addr.sin_port = htons(VIDEO_PORT);
+
     bind(audio_socket_fd, (struct sockaddr*)&audio_addr, sizeof(audio_addr));
+    bind(video_socket_fd, (struct sockaddr*)&video_addr, sizeof(video_addr));
 
     if((audio_fd = open("/dev/dsp", O_RDWR, 0644)) == -1){
         fprintf(stderr, "Failed to open /dev/dsp\n");
         exit(1);
     }
-   while(1){
-        /* ioctl(audio_fd, BLKFLSBUF, 0); */
+
+    IplImage *frame_you = 0;
+    IplImage *frame_friend = 0; //TODO: create iplimage
+
+    int32_t currentFrameYou = 0;
+    int32_t currentFrameFriend = 0;
+    srand((unsigned) time(NULL));
+    int blocksize = BUF_SIZE-sizeof(int32_t)/sizeof(char)*3;
+    //カメラの初期化（カメラの選択）
+    CvCapture *capture_you = cvCaptureFromCAM(0);
+
+    //取込サイズの設定
+    cvSetCaptureProperty (capture_you, CV_CAP_PROP_FRAME_WIDTH,  640);
+    cvSetCaptureProperty (capture_you, CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+    int cap_i = 0;
+
+    while(1){
+        ioctl(audio_fd, BLKFLSBUF, 0);
 
         FD_ZERO(&readfds);
         FD_SET(0, &readfds);
@@ -77,6 +107,7 @@ int main(int argc, char** argv){
         if(isCalling){
             FD_SET(recv_fd, &readfds);
             FD_SET(audio_socket_fd, &readfds);
+            FD_SET(video_socket_fd, &readfds);
         }
         FD_SET(audio_fd, &readfds);
 
@@ -125,7 +156,9 @@ int main(int argc, char** argv){
                 if(!isMyCalling){
                     memcpy(&other_recv_addr.sin_addr, &other_send_addr.sin_addr, sizeof(struct in_addr));
                     memcpy(&audio_addr.sin_addr, &other_send_addr.sin_addr, sizeof(struct in_addr));
+                    memcpy(&video_addr.sin_addr, &other_send_addr.sin_addr, sizeof(struct in_addr));
                     audio_addr.sin_port = htons(UDP_PORT);
+                    video_addr.sin_port = htons(VIDEO_PORT);
                     len_udp = sizeof(audio_addr);
                     if(connect(send_fd, (struct sockaddr*)&other_recv_addr, sizeof(other_recv_addr)) == -1){
                         fprintf(stderr, "Failed to connect back to %s:%d\n", other_recv_addr.sin_addr.s_addr, PORT);
@@ -167,11 +200,52 @@ int main(int argc, char** argv){
                 sendto(audio_socket_fd, buf, n, 0, (struct sockaddr*)&audio_addr, len_udp);
             }
         }
+
+        if(isCalling){
+            if(cap_i%CAP_SKIP_NUM==0){
+                //フレーム画像の取込
+                frame_you = cvQueryFrame (capture_you);
+                //画像の表示
+                cvShowImage ("You", frame_you);
+
+                currentFrameYou = rand();
+                int32_t imageSize = frame_you->imageSize;
+                char* imageData = frame_you->imageData;
+                n = imageSize/blocksize;
+                for(i=0; i<n; i++){
+                    (int32_t)buf[0] = currentFrameYou;
+                    (int32_t)buf[4] = imageSize;
+                    (int32_t)buf[8] = (int32_t)i;
+                    for(j=0; j<blocksize; j++){
+                        buf[12+j] = imageData[i*blocksize+j];
+                    }
+                    sendto(video_socket_fd, buf, BUF_SIZE, 0, (struct sockaddr*)&video_addr, len_v_udp);
+                }
+            }
+            cap_i++;
+        }
+        if(isCalling && FD_ISSET(video_socket_fd, &fds)){
+            while(n=recvfrom(video_socket_fd, buf, BUF_SIZE, 0, (struct sockaddr*)&video_addr, len_v_udp)){
+                if((int32_t)buf[0] != currentFrameYou){
+                    //changed frame
+                    cvShowImage("Friend", frame_friend);
+                }
+                frame_friend->imageSize = (int32_t)buf[4];
+                i = (int32_t)buf[8];
+                for(j=0; j<blocksize; j++){
+                    frame_friend->imageData[i*blocksize+j] = buf[12+j];
+                }
+            }
+        }
     }
 QUIT:
     close(recv_fd);
     close(send_fd);
     close(audio_fd);
+
+    cvReleaseCapture (&capture_you);
+    cvDestroyWindow ("You");
+    cvDestroyWindow ("Friend");
 
     fprintf(stdout, "Good Bye\n");
     return 0;
