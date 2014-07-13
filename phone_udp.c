3071f4218d0d4ddde5f70c48bbbf32e3098a7685
Takuma YOSHITANI <takuma.7@gmail.com>
2014-07-13 22:35:12 +0900

    fix ui

--- phone_udp.c
+++ phone_udp.c
@@ -182,11 +182,11 @@ int main(int argc, char** argv){
                     }
                     char ans = 0;
                     do{
-                        fprintf(stdout, "Take a call from %s? (y/n) >> ", inet_ntoa(other_recv_addr.sin_addr));
+                        fprintf(stdout, "Take a call from %s? (y/n) >> ", other_recv_addr.sin_addr.s_addr);
                         scanf("%c", &ans);
                     }while(ans != 'y' && ans != 'n');
                     if(ans == 'y'){
-                        fprintf(stdout, "Now you are talking with %s\n", inet_ntoa(other_recv_addr.sin_addr));
+                        fprintf(stdout, "Now you are talking with %s\n", other_recv_addr.sin_addr.s_addr);
                         isCalling = 1;
                     }else{
                         close(recv_fd);
